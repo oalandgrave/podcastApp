64c3105d2cdf4948879fd5e7f12aca074e9b4a57
omar arenas landgrave <oalandgrave@gmail.com>
2017-08-22 01:05:41 -0400

    adding collection view and table view

---
+++ podcastApp/podcastApp/SavedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SavedViewController.swift
+//  podcastApp
+//
+//  Created by omar arenas landgrave on 8/22/17.
+//  Copyright © 2017 omar arenas landgrave. All rights reserved.
+//
+
+import UIKit
+
+class SavedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
